4e60a9f64880c2cdd9d37645dffbc170d586cfd1
Jeremy Walton <jthelegoman@gmail.com>
2013-12-10 11:38:55 -0500

    Finished 2 player war game

--- Week 1/War/war_game.rb
+++ Week 1/War/war_game.rb
@@ -1,5 +1,5 @@
 class WarGame
-	attr_reader :deck, :player1, :player2
+	attr_reader :deck, :player1, :player2, :winner
 
 	def initialize(player1=WarPlayer.new, player2=WarPlayer.new)	
 		@player1, @player2 = player1, player2
@@ -15,29 +15,37 @@ class WarGame
 
 	def play_round
 		if (@player1.number_of_cards == 0)
-			puts "player 1 wins"
-		end
-		if (@player2.number_of_cards == 0)
-			pluts "player 2 wins"
-		end
-		card1 = @player1.play_top_card
-		card2 = @player2.play_top_card
-		cards = [card1, card2]
-
-		unless card1.value == card2.value
-			if (card1.value > card2.value)
-				player1.take_cards(cards)
+			@winner = "player 1"
+		else
+			if (@player2.number_of_cards == 0)
+			@winner = "player 2"
 			else
-				player2.take_cards(cards)
+				card1 = @player1.play_top_card
+				card2 = @player2.play_top_card
+				cards = [card1, card2]
+				unless card1.value == card2.value
+					if (card1.value > card2.value)
+						player1.take_cards(cards)
+					else
+						player2.take_cards(cards)
+					end
+				end
 			end
 		end
-
 	end
 
 	def play_game
 		deck.shuffle
 		deal
 		while true do
+			if (@winner == "player 1")
+				puts "player 1 wins"
+				break
+			end
+			if (@winner == "player 2")
+				puts "player 2 wins"
+				break
+			end
 			play_round
 		end
 	end
